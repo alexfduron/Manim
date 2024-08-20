1292396a4f9e3adf3428421caac21afd26c06a2b
Alex Duron <alexfduron@gmail.com>
2024-08-20 17:52:50 -0500

    Prueba exitosa

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from manim import *
+
+class prueba(Scene):
+    def construct(self):
+        cuadro = Square()
+        circulo = Circle()
+        circulo.set_fill(PINK, opacity = 0.5)
+
+        self.play(Create(cuadro))
+        self.play(Transform(cuadro, circulo))
+        self.wait()
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
