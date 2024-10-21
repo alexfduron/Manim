bda7b615a37f1c51c3f5521e311820a2ee4005e9
Alex Duron <alexfduron@gmail.com>
2024-10-20 21:16:48 -0500

    se agrego texto con movimiento

--- Clase1_Textos.py
+++ Clase1_Textos.py
@@ -6,6 +6,8 @@ from numbers import Number
 
 
 from manim import *
+from pydub.utils import FRAME_WIDTHS
+
 
 class leccion1(Scene):
     def construct(self):
@@ -96,6 +98,135 @@ class leccion7(Scene):
         self.play(Transform(texto15, texto18))
         self.wait(1)
 
+
+class Texto_Vertical(Scene):
+    def construct(self):
+
+        # agregamos las variables de altura y ancho
+        altura_actual = config.pixel_height
+        ancho_actual = config.pixel_width
+
+        config.pixel_height = ancho_actual
+        config.pixel_width = altura_actual
+
+
+        config.frame_width = config.frame_height * 9 / 16
+        #FRAME_HEIGHT = config.frame_height
+        #FRAME_WIDTH = config.frame_width
+
+        rejilla = NumberPlane()
+        rejilla.scale(0.5)
+
+        # agregamos el texto
+        txt1 = Text("+", color=YELLOW)
+        txt2 = Text("UP", color=RED).move_to(UP)
+        txt3 = Text("2UP", color=GREEN).move_to(UP*2)
+        txt4 = Text("3UP", color=BLUE).move_to(UP*3)
+        txt5 = Text("4UP", color=ORANGE).move_to(UP*4)
+
+        txt6 = Text("DOWN", color=RED).move_to(DOWN)
+        txt7 = Text("2DOWN", color=GREEN).move_to(DOWN*2)
+        txt8 = Text("3DOWN", color=BLUE).move_to(DOWN*3)
+        txt9 = Text("4DOWN", color=ORANGE).move_to(DOWN*4)
+
+
+        # simular
+        self.camera.background_color=BLUE_E
+        self.add(rejilla)
+        self.wait()
+        self.play(
+            FadeIn(txt1),
+            FadeIn(txt2),
+            FadeIn(txt3),
+            FadeIn(txt4),
+            FadeIn(txt5)
+        )
+        self.play(
+            FadeIn(txt6),
+            FadeIn(txt7),
+            FadeIn(txt8),
+            FadeIn(txt9)
+        )
+
+        self.wait(1)
+
+
+
+class texto_2(Scene):
+    def construct(self):
+
+        rejilla = NumberPlane()
+
+        txt1 =  Text("Formula animada con Manim")
+        txt1.scale(1.2)
+        txt1[17:22].set_color(GREEN)
+        txt1.move_to(UP * 2.5) # se usa para mover desde el centro
+        #txt1.to_corner(DL, buff=2)    # se usa para mover a la esquina
+        #txt1.to_edge(UP, buff=1)      # se usa para mover al borde
+
+        txt2 = MathTex("v","=","{d \\over dt}","\\left(","A"," \\sin \\left(\\omega t + \\phi \\right)","\\right)")
+        txt2.scale(0.7)
+        txt2.to_edge(LEFT, buff=5)
+
+        txt3=txt2.copy()
+        txt3.move_to(UP).to_edge(LEFT, buff=5)
+
+        txt4 = MathTex("v","=","A","{d \\over dt}","\\left("," \\sin \\left(\\omega t + \\phi \\right)","\\right)")
+        txt4.scale(0.7)
+        txt4.to_edge(LEFT, buff=5)
+
+        txt5 = MathTex("v","=","A"," \\cos \\left(\\omega t + \\phi \\right)","{d \\over dt}","\\left(\\omega t + \\phi \\right)")
+        txt5.scale(0.7)
+        txt5.move_to(DOWN).to_edge(LEFT, buff=5)
+
+        txt6 = MathTex("v","=","A","\\omega"," \\cos \\left(\\omega t + \\phi \\right)")
+        txt6.scale(0.7)
+        txt6.move_to(DOWN * 2).to_edge(LEFT, buff=5)
+
+        #self.add(rejilla)
+        self.play(Write(txt1), Write(txt2))
+
+        self.play(Transform(txt2,txt3))
+
+        self.play(
+            Transform(txt3[0:2].copy(), txt4[0:2])
+        )
+        self.play(
+            Transform(txt3[4].copy().set_color(YELLOW), txt4[2])
+        )
+        self.play(
+            Transform(txt3[2:4].copy(), txt4[3:5]),
+            Transform(txt3[5:7].copy(), txt4[5:7])
+        )
+
+        self.play(
+            Transform(txt4[0:2].copy(), txt5[0:2])
+        )
+        self.play(
+            Transform(txt4[2].copy(), txt5[2])
+        )
+        self.play(
+            Transform(txt4[5].copy().set_color(YELLOW), txt5[3])
+        )
+        self.play(
+            Transform(txt4[3].copy(), txt5[4]),
+            Transform(txt4[5].copy(), txt5[5])
+        )
+
+        self.play(
+            Transform(txt5[0:2].copy(), txt6[0:2])
+        )
+        self.play(
+            Transform(txt5[2].copy(), txt6[2])
+        )
+        self.play(
+            Transform(txt5[4:6].copy().set_color(YELLOW), txt6[3])
+        )
+        self.play(
+            Transform(txt5[3].copy(), txt6[4])
+        )
+
+        self.wait(2)
 
 
 
